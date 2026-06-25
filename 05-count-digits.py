b996b6b9bc7db409f24477e4ac0f9bb536ac7831
Dang Bao Trung <dangbaotrung2008@gmail.com>
2026-06-25 18:23:12 +0700

    05-count-digits.py

---
+++ 05-count-digits.py
@@ -0,0 +1,9 @@
+n = int(input())
+count = 0
+if n == 0:
+    count = 1
+else:
+    while n != 0:
+        n = n // 10
+        count += 1
+print (count)
--- 05-sum-of-digits.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-total = 0
-while n != 0:
-    total += n % 10
-    n = n // 10
-print(total)
